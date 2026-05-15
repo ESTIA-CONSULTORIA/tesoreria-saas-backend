631241fe9011d976ac9fb91de20b253409a881cc
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-15 11:36:40 -0700

    Register sync worker

--- src/sync-queue/sync-queue.module.ts
+++ src/sync-queue/sync-queue.module.ts
@@ -4,11 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SyncQueue } from './entities/sync-queue.entity';
 import { SyncQueueService } from './sync-queue.service';
 import { SyncQueueController } from './sync-queue.controller';
+import { SyncWorkerService } from './sync-worker.service';
+import { SyncWorkerController } from './sync-worker.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SyncQueue])],
-  providers: [SyncQueueService],
-  controllers: [SyncQueueController],
-  exports: [SyncQueueService],
+  providers: [SyncQueueService, SyncWorkerService],
+  controllers: [SyncQueueController, SyncWorkerController],
+  exports: [SyncQueueService, SyncWorkerService],
 })
 export class SyncQueueModule {}
