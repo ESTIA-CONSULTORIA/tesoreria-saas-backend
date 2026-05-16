b5c69e3387aab77cb02e528932bc0bf00b4ca322
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-16 07:17:27 -0700

    Add realtime websocket gateway

---
+++ src/realtime/realtime.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class RealtimeGateway {
+  @WebSocketServer()
+  server: Server;
+
+  emitEvent(event: string, payload: any) {
+    this.server.emit(event, payload);
+  }
+}
