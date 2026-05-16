6ce8200e52158fcec4a382f5aeba120bb4b842dd
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-15 22:31:19 -0700

    Register sales financial and reporting services

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -5,10 +5,16 @@ import { Sale } from './entities/sale.entity';
 import { SaleItem } from './entities/sale-item.entity';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
+import { SalesFinancialService } from './services/sales-financial.service';
+import { SalesReportingService } from './services/sales-reporting.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Sale, SaleItem])],
-  providers: [SalesService],
+  providers: [
+    SalesService,
+    SalesFinancialService,
+    SalesReportingService,
+  ],
   controllers: [SalesController],
   exports: [SalesService],
 })
