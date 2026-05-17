9896477b05e85aa9211bf7e6c9efa1c8dc1bee57
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-17 16:44:50 -0700

    Add base organization seed

---
+++ src/database/seeds/base-organization.seed.ts
@@ -0,0 +1,16 @@
+export const baseTenant = {
+  id: 'default-tenant',
+  name: 'Default Tenant',
+};
+
+export const baseCompany = {
+  id: 'default-company',
+  tenantId: 'default-tenant',
+  name: 'Default Company',
+};
+
+export const baseBranch = {
+  id: 'default-branch',
+  companyId: 'default-company',
+  name: 'Sucursal Principal',
+};
