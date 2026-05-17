495708a3aa619dc918da7f327c220270b1abe51b
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-17 14:50:50 -0700

    Connect movements module into treasury module

--- src/treasury/treasury.module.ts
+++ src/treasury/treasury.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { MovementsModule } from '../movements/movements.module';
 import { TreasuryMovementService } from './services/treasury-movement.service';
 import { TreasuryForecastService } from './services/treasury-forecast.service';
 import { TreasuryRiskAlertService } from './services/treasury-risk-alert.service';
 
 @Module({
+  imports: [MovementsModule],
   providers: [
     TreasuryMovementService,
     TreasuryForecastService,
