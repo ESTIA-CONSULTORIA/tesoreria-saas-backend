84634f00a3c84f124ef59d33818478f530d6308f
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-16 23:49:14 -0700

    Register realtime gateway in realtime module

--- src/realtime/realtime.module.ts
+++ src/realtime/realtime.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { RealtimeGateway } from './gateways/realtime.gateway';
 import { DashboardBroadcastService } from './services/dashboard-broadcast.service';
 import { KpiStreamService } from './services/kpi-stream.service';
 
 @Module({
   providers: [
+    RealtimeGateway,
     DashboardBroadcastService,
     KpiStreamService,
   ],
   exports: [
+    RealtimeGateway,
     DashboardBroadcastService,
     KpiStreamService,
   ],
