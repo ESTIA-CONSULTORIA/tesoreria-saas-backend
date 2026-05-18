e2110692a23f30f6ce0f227c457f58f7b9727179
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-18 06:42:02 -0700

    Use validated plan DTO

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { PlansService } from './plans.service';
-import { Feature } from '../auth/feature/decorator';
-import { Plan } from './entities/plan.entity';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+} from '@nestjs/common';
 
+import { Feature } from '../auth/feature/decorator';
+import { CreatePlanDto } from './dto/create-plan.dto';
+import { PlansService } from './plans.service';
 
 @Controller('plans')
 export class PlansController {
   constructor(private plansService: PlansService) {}
 
   @Post()
-  create(@Body() body: Partial<Plan>) {
+  create(@Body() body: CreatePlanDto) {
     return this.plansService.create(body);
   }
 
