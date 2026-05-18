ccab209d62963b14ee2cbf8df30c174bfc54a244
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-17 18:36:11 -0700

    Run SaaS plans seed during bootstrap

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 import { runBaseOrganizationSeed } from './database/seeds/run-base-organization.seed';
 import { runBaseFinancialSeed } from './database/seeds/run-base-financial.seed';
 import { runBaseSubscriptionSeed } from './database/seeds/run-base-subscription.seed';
+import { runBasePlansSeed } from './database/seeds/run-base-plans.seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -27,6 +28,7 @@ async function bootstrap() {
 
   await runBaseOrganizationSeed(dataSource);
   await runBaseFinancialSeed(dataSource);
+  await runBasePlansSeed(dataSource);
   await runBaseSubscriptionSeed(dataSource);
 
   await app.listen(process.env.PORT ?? 3000);
