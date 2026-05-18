13a28ef82eebcfa16457aab8f678a4aad0958fd4
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-17 18:29:49 -0700

    Add base SaaS plans seed

---
+++ src/database/seeds/base-plans.seed.ts
@@ -0,0 +1,17 @@
+export const basePlans = [
+  {
+    code: 'BASIC',
+    name: 'Basic',
+    monthlyPrice: 29,
+  },
+  {
+    code: 'PRO',
+    name: 'Professional',
+    monthlyPrice: 99,
+  },
+  {
+    code: 'ENTERPRISE',
+    name: 'Enterprise',
+    monthlyPrice: 299,
+  },
+];
