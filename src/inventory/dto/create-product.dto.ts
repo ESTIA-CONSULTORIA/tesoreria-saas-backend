1ad8a4bdebc9df5b151bbd33da4f3397f16dc006
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-17 21:19:24 -0700

    Add validated inventory product DTO

---
+++ src/inventory/dto/create-product.dto.ts
@@ -0,0 +1,19 @@
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  sku?: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  stock?: number;
+}
