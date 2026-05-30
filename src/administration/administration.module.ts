f54a4c3a37edb9f83bb38f3b869831355aa6df4f
ESTIA-CONSULTORIA <loraloraangel@gmail.com>
2026-05-30 10:11:28 -0700

    Agregar TenantSetting a TypeOrmModule.forFeature en AdministrationModule para resolver error de dependencias

--- src/administration/administration.module.ts
+++ src/administration/administration.module.ts
@@ -4,9 +4,10 @@ import { AdministrationService } from './administration.service';
 import { AdministrationController } from './administration.controller';
 import { AuditLog } from './entities/audit-log.entity';
 import { Tenant } from '../tenants/entities/tenant.entity';
+import { TenantSetting } from '../tenant-settings/entities/tenant-setting.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuditLog, Tenant])],
+  imports: [TypeOrmModule.forFeature([AuditLog, Tenant, TenantSetting])],
   controllers: [AdministrationController],
   providers: [AdministrationService],
   exports: [AdministrationService],
